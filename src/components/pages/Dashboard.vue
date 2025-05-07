2a468ade13ffba4a1c5c006cbfbdb21c80b04800
Dilson <dilsono316@gmail.com>
2025-05-07 16:24:02 -0300

    dashboard building

--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -18,9 +18,18 @@ onMounted(() => {
         <div class="container">
             <p>Olá <strong>{{userName}}</strong> Seja bem-vindo!</p>
         </div>
-        <div>
-            
-        </div>
+        <section class="container box_fit">
+            <a href="" class="link_fit">
+                <div class="box_fit_links">
+                    <img class="box_img" src="../../assets/images/training.png" alt="treinando">
+                    <h2 class="fit_title">Treinos</h2>
+                </div>
+            </a>
+            <div class="box_fit_links">
+            </div>
+            <div class="box_fit_links">
+            </div>
+        </section>
     </section>
 </template>
 
@@ -28,5 +37,47 @@ onMounted(() => {
     .container{
         max-width: 1280px;
         margin: auto;
+    }
+    .box_fit{
+        display: flex;
+        justify-content: space-between;
+    }
+    .box_fit_links{
+
+
+box-sizing: border-box;
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 12px 40px;
+gap: 10px;
+
+width: 371px;
+height: 281px;
+
+/* primary/153B4E */
+background: #153B4E;
+border: 1px solid #000000;
+border-radius: 10px;
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+    }
+    .box_img{
+        width: 100%;
+    }
+    .fit_title{
+        color: white;
+      
+    }
+    .link_fit{
+        text-decoration: none;
+        font-size: 16px;
     }
 </style>
