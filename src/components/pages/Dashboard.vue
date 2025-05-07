31e1bc00a7cbdc2770e4980302797373665a2ced
Dilson <dilsono316@gmail.com>
2025-05-07 16:54:06 -0300

    links fit

--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -2,6 +2,7 @@
 import Header from '../layouts/Header.vue';
 import Navbar from '../layouts/Navbar.vue';
 import { ref, onMounted } from 'vue'
+import LinkFit from '../ui/LinkFit.vue';
 
 const userName = ref('')
 
@@ -19,16 +20,10 @@ onMounted(() => {
             <p>Olá <strong>{{userName}}</strong> Seja bem-vindo!</p>
         </div>
         <section class="container box_fit">
-            <a href="" class="link_fit">
-                <div class="box_fit_links">
-                    <img class="box_img" src="../../assets/images/training.png" alt="treinando">
-                    <h2 class="fit_title">Treinos</h2>
-                </div>
-            </a>
-            <div class="box_fit_links">
-            </div>
-            <div class="box_fit_links">
-            </div>
+            <LinkFit image="../../assets/images/training.png" title="Treinos"/>
+            <LinkFit image="../../assets/images/diet.png" title="Plano nutricional"/>
+            <LinkFit image="../../assets/images/progress.png" title="Progresso"/>
+            
         </section>
     </section>
 </template>
@@ -41,43 +36,8 @@ onMounted(() => {
     .box_fit{
         display: flex;
         justify-content: space-between;
+        flex-wrap: wrap;
+        margin-top: 20px;
     }
-    .box_fit_links{
-
-
-box-sizing: border-box;
-
-/* Auto layout */
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-padding: 12px 40px;
-gap: 10px;
-
-width: 371px;
-height: 281px;
-
-/* primary/153B4E */
-background: #153B4E;
-border: 1px solid #000000;
-border-radius: 10px;
-
-/* Inside auto layout */
-flex: none;
-order: 0;
-flex-grow: 0;
-
-    }
-    .box_img{
-        width: 100%;
-    }
-    .fit_title{
-        color: white;
-      
-    }
-    .link_fit{
-        text-decoration: none;
-        font-size: 16px;
-    }
+  
 </style>
