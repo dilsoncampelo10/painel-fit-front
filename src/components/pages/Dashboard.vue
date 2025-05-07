3f27089535e71b08f49dc166e34f1169b8d19b8c
Dilson <dilsono316@gmail.com>
2025-05-07 17:14:08 -0300

    cards

--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -3,6 +3,7 @@ import Header from '../layouts/Header.vue';
 import Navbar from '../layouts/Navbar.vue';
 import { ref, onMounted } from 'vue'
 import LinkFit from '../ui/LinkFit.vue';
+import CardFit from '../ui/CardFit.vue';
 
 const userName = ref('')
 
@@ -25,6 +26,13 @@ onMounted(() => {
             <LinkFit image="../../assets/images/progress.png" title="Progresso"/>
             
         </section>
+
+        <section class="container box_fit card_area">
+            <CardFit title="Treinos concluídos / Semana" :number="3"/>
+            <CardFit title="Treinos concluídos / Semana" :number="5"/>
+            <CardFit title="Treinos concluídos / Semana" :number="2"/>
+            
+        </section>
     </section>
 </template>
 
@@ -38,6 +46,11 @@ onMounted(() => {
         justify-content: space-between;
         flex-wrap: wrap;
         margin-top: 20px;
+        gap: 30px;
     }
+    .card_area{
+        margin-top: 130px;
+    }
+ 
   
 </style>
